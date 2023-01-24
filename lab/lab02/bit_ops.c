c89698beedc900de986ff6d7ce0736ed06c54e0a
ruiyan-ma <maruiyan17@gmail.com>
2023-01-23 22:38:07 -0800

    finish lab02

--- lab/lab02/bit_ops.c
+++ lab/lab02/bit_ops.c
@@ -4,24 +4,34 @@
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
-                 unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
-    // 0 or 1)
-    return -1;
+                 unsigned n)
+{
+    x = x >> n;
+    return x & 1;
 }
+
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
-             unsigned v) {
-    // YOUR CODE HERE
+             unsigned v)
+{
+    // x = x & 11..101..11 (0 is the nth bit)
+    // x = xx..0..xx (0 is the n bit)
+    *x = *x & ~(1 << n);
+
+    // x = x | 00..0v0..00 (v is the nth bit)
+    // x = xx..v..xx (v is the nth bit)
+    *x = *x | (v << n);
 }
+
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
-              unsigned n) {
-    // YOUR CODE HERE
+              unsigned n)
+{
+    // x = xx..v..xx ^ 00..1..0
+    // x ^ 0 = x
+    // v ^ 1 = ~v
+    *x = *x ^ (1 << n);
 }
-
--- lab/lab02/lfsr.c
+++ lab/lab02/lfsr.c
@@ -4,7 +4,9 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+void lfsr_calculate(uint16_t *reg)
+{
+    uint16_t bit = ((*reg) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1;
+    *reg = *reg >> 1;
+    *reg = *reg | (bit << 15);
 }
-
