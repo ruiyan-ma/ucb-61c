e6b88ff3aff4d53ab96fd6cd2b524361b7fbd68c
ruiyan-ma <maruiyan17@gmail.com>
2023-02-01 21:20:27 -0800

    finish proj2 part 1

--- proj2/unittests/unittests.py
+++ proj2/unittests/unittests.py
@@ -188,16 +188,20 @@ class TestMatmul(TestCase):
         array_out = t.array([0] * len(result))
 
         # load address of input matrices and set their dimensions
-        raise NotImplementedError("TODO")
-        # TODO
-        # load address of output array
-        # TODO
+        t.input_array("a0", array0)
+        t.input_scalar("a1", m0_rows)
+        t.input_scalar("a2", m0_cols)
 
+        t.input_array("a3", array1)
+        t.input_scalar("a4", m1_rows)
+        t.input_scalar("a5", m1_cols)
+        # load address of output array
+        t.input_array("a6", array_out)
         # call the matmul function
         t.call("matmul")
 
         # check the content of the output array
-        # TODO
+        t.check_array(array_out, result)
 
         # generate the assembly file and run it through venus, we expect the simulation to exit with code `code`
         t.execute(code=code)
@@ -208,6 +212,17 @@ class TestMatmul(TestCase):
             [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
             [30, 36, 42, 66, 81, 96, 102, 126, 150]
         )
+
+    def test_m0_dimension(self):
+        self.do_matmul([1], 0, 1, [1], 1, 1, [1], 72)
+        self.do_matmul([1], 1, -1, [1], 1, 1, [1], 72)
+
+    def test_m1_dimension(self):
+        self.do_matmul([1], 1, 1, [1], 0, 1, [1], 73)
+        self.do_matmul([1], 1, 1, [1], 1, -1, [1], 73)
+
+    def test_dimension_mismatch(self):
+        self.do_matmul([1], 1, 1, [1, 2], 2, 1, [2], 74)
 
     @classmethod
     def tearDownClass(cls):
