e1250dd1ee93f4432c0a83c6ef044bd9365a7d16
ruiyan-ma <maruiyan17@gmail.com>
2023-02-02 13:10:19 -0800

    finish proj2 read and write matrix.

--- proj2/src/classify.s
+++ proj2/src/classify.s
@@ -21,40 +21,64 @@ classify:
     # Usage:
     #   main.s <M0_PATH> <M1_PATH> <INPUT_PATH> <OUTPUT_PATH>
 
+    # prologue
+    addi sp, sp, 0
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    
+    # save arguments
+    mv s0, a0  # argc
+    mv s1, a1  # argv
+    mv s2, a2  # print flag
 
-
-
-
+    # check arguments
+    jal check_arguments
 
 	# =====================================
     # LOAD MATRICES
     # =====================================
 
+    # allocate memory for m0.rows and m0.cols
+    li a0, 8  # bytes to allocate
+    jal malloc
+    jal check_malloc_error
+    mv s3, a0  # pointer to (m0.rows, m0.cols)
 
+    # load pretrained m0
+    lw a0, 4(s1)  # pointer to filename
+    addi a1, s3, 0  # pointer to row number
+    addi a2, s3, 4  # pointer to column number
+    jal read_matrix
+    mv s4, a0  # pointer to m0
 
+    # allocate memory for m1.rows and m1.cols
+    li a0, 8  # bytes to allocate
+    jal malloc
+    jal check_malloc_error
+    mv s5, a0  # pointer to (m0.rows, m0.cols)
 
+    # load pretrained m1
+    lw a0, 8(s1)  # pointer to filename
+    addi a1, s5, 0  # pointer to row number
+    addi a2, s5, 4  # pointer to column number
+    jal read_matrix
+    mv s6, a0  # pointer to m0
 
+    # allocate memory for input.rows and input.cols
+    li a0, 8  # bytes to allocate
+    jal malloc
+    jal check_malloc_error
+    mv s7, a0  # pointer to (m0.rows, m0.cols)
 
-    # Load pretrained m0
-
-
-
-
-
-
-    # Load pretrained m1
-
-
-
-
-
-
-    # Load input matrix
-
-
-
-
-
+    # load input matrix
+    lw a0, 12(s1)  # pointer to filename
+    addi a1, s7, 0  # pointer to row number
+    addi a2, s7, 4  # pointer to column number
+    jal read_matrix
+    mv s8, a0  # pointer to m0
+   
 
     # =====================================
     # RUN LAYERS
@@ -64,45 +88,82 @@ classify:
     # 3. LINEAR LAYER:    m1 * ReLU(m0 * input)
 
 
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
     # =====================================
     # WRITE OUTPUT
     # =====================================
     # Write output matrix
-
-
-
-
 
     # =====================================
     # CALCULATE CLASSIFICATION/LABEL
     # =====================================
     # Call argmax
 
-
-
-
     # Print classification
-    
-
-
 
     # Print newline afterwards for clarity
 
+    ret
 
+#================================================================
+# int** read_matrix_info(char* a1)
+# Allocate memory for rows and cols, and load matrix.
+# args:
+#   a1 = pointer to filename
+# return:
+#   a0 = (pointer to (rows, cols), pointer to matrix)
+#================================================================
+read_matrix_info:
+    # prologue
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
 
+    # save argument
+    mv s1, a1
+
+    # allocate memory for rows and cols
+    li a0, 8  # bytes to allocate
+    jal malloc
+    jal check_malloc_error
+    mv s0, a0  # pointer to (rows, cols)
+
+    # load matrix
+    mv a0, s1  # pointer to filename
+    addi a1, s0, 0  # pointer to row number
+    addi a2, s0, 4  # pointer to column number
+    jal read_matrix
+    mv s1, a0  # pointer to m0
+ 
+    # allocate memory for ((rows, cols), matrix)
+    li a0, 8
+    jal malloc
+    jal check_malloc_error
+    sw s0, 0(a0)
+    sw s1, 4(a0)
+
+    # epilogue
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
 
     ret
+
+
+check_arguments:
+    li t0, 5
+    li a1, 89
+    bne a0, t0, exit_with_error
+    ret
+
+
+check_malloc_error:
+    li a1, 88
+    beq a0, x0, exit_with_error
+    ret
+
+
+exit_with_error:
+    j exit2
+
--- proj2/src/read_matrix.s
+++ proj2/src/read_matrix.s
@@ -27,16 +27,115 @@
 read_matrix:
 
     # Prologue
-	
+    addi sp, sp, -28
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
 
+    # save arguments
+    mv s0, a0  # pointer to filename
+    mv s1, a1  # pointer for row number
+    mv s2, a2  # pointer for column number
 
+    # open file
+    mv a1, s0  # filepath
+    li a2, 0  # permission: read only
+    jal fopen
+    jal check_fopen_error
+    # save file descriptor
+    mv s0, a0
 
+    # read row number
+    mv a1, s0  # file descriptor
+    mv a2, s1  # write the read bytes to s1
+    li a3, 4  # number of bytes to be read
+    jal fread
+    jal check_fread_error
 
+    # read column number
+    mv a1, s0  # file descriptor
+    mv a2, s2  # write the read bytes to s2
+    li a3, 4  # number of bytes to be read
+    jal fread
+    jal check_fread_error
 
+    # allocate memory
+    lw t0, 0(s1)  # row number
+    lw t1, 0(s2)  # column number
+    mul s3, t0, t1  # element number
+    slli a0, s3, 2
+    jal malloc
+    jal check_malloc_error
+    # save memory pointer
+    mv s4, a0
 
+    # read matrix
+    li s5, 0  # i = 0
+read_loop:
+    beq s5, s3, loop_end
 
+    # set current memory pointer
+    slli t0, s5, 2
+    add t0, t0, s4
+
+    # read next element
+    mv a1, s0  # file descriptor
+    mv a2, t0  # write the read bytes to t0
+    li a3, 4  # number of bytes to be read
+    jal fread
+    jal check_fread_error
+
+    # i += 1
+    addi s5, s5, 1
+
+    j read_loop
+
+loop_end:
+    # close file
+    mv a1, s0  # file descriptor
+    jal fclose
+    jal check_fclose_error
+
+    # set return value
+    mv a0, s4
 
     # Epilogue
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    addi sp, sp, 28
 
     ret
+
+check_malloc_error:
+    li a1, 88
+    beq a0, x0, exit_with_error
+    ret
+
+check_fopen_error:
+    li t0, -1
+    li a1, 90
+    beq a0, t0, exit_with_error
+    ret
+
+check_fread_error:
+    li t0, 4
+    li a1, 91
+    bne a0, t0, exit_with_error
+    ret
+
+check_fclose_error:
+    li a1, 92
+    bne a0, x0, exit_with_error
+    ret
+
+exit_with_error:
+    j exit2
